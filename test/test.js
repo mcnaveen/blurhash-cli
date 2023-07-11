c1947e0d5e218c36c2c83ea2f5d10c4271b26e75
mcnaveen <8493007+mcnaveen@users.noreply.github.com>
2023-07-11 15:41:04 +0530

    fix test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
 import test from "ava";
 import { execa } from "execa";
 
-test("https://i.imgur.com/NhfEdg2.png", async (t) => {
+test("Blurhash generation from URL", async (t) => {
   await execa("node", ["cli.js", "https://i.imgur.com/NhfEdg2.png"]);
   t.pass();
 });
 
-test("./image/cover.png --size=64 --local", async (t) => {
+test("Blurhash generation from local image", async (t) => {
   await execa("node", ["cli.js", "./image/cover.png", "--size=64", "--local"]);
   t.pass();
 });
